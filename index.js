eecfed750f1bcaffcc34708cf37f9f8636e303cf
Brandon Graham <bgraham452@gmail.com>
2018-09-07 11:59:20 -0400

    added delete enpoint and get by index endpoint for cats and dogs

--- index.js
+++ index.js
@@ -8,6 +8,71 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+
+let cats = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Whiskers',
+    sex: 'Male',
+    age: 3,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Queenie',
+    sex: 'Female',
+    age: 1,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+];
+
+let dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Athena',
+    sex: 'Female',
+    age: 2,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Dutchie',
+    sex: 'Male',
+    age: 1,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+
+];
+
+
 const app = express();
 
 app.use(
@@ -21,6 +86,55 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.get('/api/cats', (req, res, next) => {
+  return res.status(200).json(cats);
+});
+
+app.get('/api/dogs', (req,res,next) => {
+  return res.status(200).json(dogs);
+});
+
+app.get('/api/cats/:index', (req, res, next) => {
+  const {index} = req.params;
+  return res.status(200).json(cats[index]);
+});
+
+app.get('/api/dogs/:index', (req,res,next) => {
+  const {index} = req.params;
+
+  return res.status(200).json(dogs[index]);
+});
+
+app.delete('/api/cats', (req, res, next) => {
+
+  let remainingCats = [];
+  for (let i = 1; i < cats.length; i++){
+    remainingCats.push (cats[i]);
+  }
+
+  console.log(remainingCats);
+  cats = remainingCats;
+  return res.status(204).json(cats);
+
+});
+
+app.delete('/api/dogs', (req, res, next) => {
+
+  let remainingDogs = [];
+  for (let i = 1; i < dogs.length; i++){
+    remainingDogs.push (dogs[i]);
+  }
+
+  console.log(remainingDogs);
+  dogs = remainingDogs;
+  return res.status(204).json(dogs);
+
+});
+
+
+
+
 
 function runServer(port = PORT) {
   const server = app
